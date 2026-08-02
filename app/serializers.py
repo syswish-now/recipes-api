6c237281f5ae9705f4ab9d296309b6a0744feab1
syswish-now <gauthy848@gmail.com>
2026-08-01 22:58:04 -0700

    changed serializers and models (#1)
    * changed serializers and models
    
    * added a comment to serializer.py
    
    ---------
    
    Co-authored-by: gauthy <syswish.llc@gmail.com>

--- app/serializers.py
+++ app/serializers.py
@@ -1,9 +1,19 @@
 from rest_framework import serializers
+from .models import Recipe, RecipeRating
 
-from .models import Recipe
-
-
+# This is the change that was made and its very import comment
 class RecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipe
         fields = ["id", "title", "description", "time_minutes", "price"]
+
+class RecipeRatingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RecipeRating
+        fields = ["id", "stars", "created_at"]
+        read_only_fields = ["id", "created_at"]
+
+    def validate_stars(self, value):
+        if not (1 <= value <= 5):
+            raise serializers.ValidationError("Rating must be between 1 and 5.")
+        return value
